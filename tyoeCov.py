9d9c1f31ae3990b9e44715a296f5203ff3074e0a
Riju Karmakar_7 <101019184+KARMAKAR7@users.noreply.github.com>
2025-07-30 00:40:15 +0530

    first commit

---
+++ tyoeCov.py
@@ -0,0 +1,4 @@
+a = "a"
+print(ord(a)) # unicode of the char c
+
+print(chr(97)) # convert unicode to char
---
+++ varibale.py
@@ -0,0 +1,6 @@
+a = 12
+b = 2/4 # float
+c = 4.4 # float
+d = True # boolean
+e = 78j # complex Number
+e = "hey" #string
